8a838a59f866202f3602a5c14114f2c74f8525d4
Sumit Savla <susavla@paypal.com>
2013-10-12 03:37:09 -0700

    improved tab menu, addded slide out menu

--- js/app/app.js
+++ js/app/app.js
@@ -10,7 +10,18 @@ $(function() {
 		$("#pageHeader h1").html(segmentedOptions.labels[selectedPanel]);
 	};
 	var newSegmented = $.UICreateSegmented(segmentedOptions);
-	//$('#segmentedPanel').append(newSegmented);
 	$('.segmented').UISegmented({callback:segmentedResponse});
 	$('.segmented').UIPanelToggle('#toggle-panels',function(){$.noop;});
+
+	$.UISlideout();
+	$('.slide-out > section').append(
+		'<h2>Your Stuff</h2>\
+		<ul class="list">\
+			<li data-show-article="music"><h3>Music</h3></li>\
+			<li data-show-article="pictures"><h3>Pictures</h3></li>\
+			<li data-show-article="recipes"><h3>Recipes</h3></li>\
+			<li data-show-article="contacts"><h3>Contacts</h3></li>\
+			</ul>'
+	);
+	
 });
